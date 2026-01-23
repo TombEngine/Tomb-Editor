d8c8c262971a719950d8c40b27cf04dd93efeade
Lwmte <3331699+Lwmte@users.noreply.github.com>
2026-01-23 20:05:36 +0100

    Update PathfindingDecompiled.cs

--- TombLib/TombLib/LevelData/Compilers/TombEngine/PathfindingDecompiled.cs
+++ TombLib/TombLib/LevelData/Compilers/TombEngine/PathfindingDecompiled.cs
@@ -742,9 +742,14 @@ namespace TombLib.LevelData.Compilers.TombEngine
             else
             {
                 // Duplicate found - update flags if needed
-                dec_boxes[boxIndex].Flipped |= box.Flipped;
-                dec_boxes[boxIndex].Water   |= box.Water;
-                dec_boxes[boxIndex].Shallow |= box.Shallow;
+                if (box.Flipped)
+                    dec_boxes[boxIndex].Flipped = true;
+
+                if (box.Water)
+                    dec_boxes[boxIndex].Water = true;
+
+                if (box.Shallow)
+                    dec_boxes[boxIndex].Shallow = true;
             }
 
             return boxIndex;
