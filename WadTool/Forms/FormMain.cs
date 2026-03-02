d415dff3e3d79db15a2b0176ebb38cf0dbf31ba8
Lwmte <3331699+Lwmte@users.noreply.github.com>
2026-03-02 01:38:48 +0100

    Update FormMain.cs

--- WadTool/Forms/FormMain.cs
+++ WadTool/Forms/FormMain.cs
@@ -551,18 +551,6 @@ namespace WadTool
             WadActions.EditObject(_tool, this, DeviceManager.DefaultDeviceManager);
         }
 
-        private void butEditLuaProperties_Click(object sender, EventArgs e)
-        {
-            // From toolbar or context menu: focus on selected item
-            var sel = _tool.MainSelection;
-            IWadObjectId focusId = null;
-            if (sel.HasValue && sel.Value.WadArea == WadArea.Destination &&
-                (sel.Value.Id is WadMoveableId || sel.Value.Id is WadStaticId))
-                focusId = sel.Value.Id;
-
-            WadActions.EditLuaProperties(_tool, this, focusId);
-        }
-
         private void itemPropertiesToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // From Tools menu: open without focusing on any specific item
